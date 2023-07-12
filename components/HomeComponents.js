821b6c332e9f9e6bc21fca30f997ef613bfa1c83
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-07-12 15:02:10 +0700

    Initial commit

---
+++ components/HomeComponents.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const HomeComponents = props => {
+    return (
+        <View>
+            <Text>{props.testing}</Text>
+        </View>
+    )
+}
+
+export default HomeComponents;
