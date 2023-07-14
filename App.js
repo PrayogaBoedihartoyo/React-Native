e904c7687c3cf728601f610920aeb98cc6ad863c
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-07-14 15:22:34 +0700

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import HomeComponents from './components/HomeComponents';
 
 export default function App() {
+  const [testing, setTesting] = useState('');
   return (
     <View style={styles.container}>
         <HomeComponents testing="Hello World! ini dari home components"/>
+        <TextInput
+        style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={text => setTesting(text)}
+        />
+        <Text>{testing}</Text>
       <StatusBar style="auto" />
     </View>
   );
